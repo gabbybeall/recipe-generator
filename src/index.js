6b4e64b2007f0df4aa1ec7dcb5933d11f8e79e06
gabbybeall <gabriellebeall@gmail.com>
2025-09-04 12:20:04 -0700

    Added basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = "The recipe";
+}
+
+let recipeForm = document.querySelector("#recipe-generator-form");
+recipeForm.addEventListener("submit", generateRecipe);
